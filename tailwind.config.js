4cec4c692a18ef42d0a88d86e663bc35267d6fe4
[Jacob Kouns] <[jacob.kouns01@gmail.com]>
2023-01-23 19:50:51 -0600

    start of tailwind.js file

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    theme: {
+        spacing: {
+            '1': '8px',
+            '2': '12px',
+            '3': '16px',
+            '4': '24px',
+            '5': '32px',
+            '6': '48px',
+          },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        'red': '#7f1d1d',
+        'white': '#e7e5e4',
+        'black': '#171717',
+        
+      },
+      fontFamily1: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      fontFamily2:{
+        sans:['Explora', 'cursive'],
+
+      },
+      extend: {
+        spacing: {
+          '128': '32rem',
+          '144': '36rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        }
+      }
+    }
+  }
